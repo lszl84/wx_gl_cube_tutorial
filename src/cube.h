004b7366ae9edb03f9fcc4cbe85423f66c5eaca0
Luke <luke@justdevtutorials.com>
2023-09-13 22:43:50 +0200

    two sides of the cube

--- src/cube.h
+++ src/cube.h
@@ -5,13 +5,27 @@
 struct Cube
 {
     static constexpr auto xyzArray = std::array{
-        -0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
-        0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
-        0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
-        -0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f};
+        // front
+        -0.5f, -0.5f, -0.5f, 1.0f, 0.0f, 0.0f, // 0
+        0.5f, -0.5f, -0.5f, 1.0f, 0.0f, 0.0f,  // 1
+        0.5f, 0.5f, -0.5f, 1.0f, 0.0f, 0.0f,   // 2
+        -0.5f, 0.5f, -0.5f, 1.0f, 0.0f, 0.0f,  // 3
+
+        // top
+        -0.5f, 0.5f, -0.5f, 0.0f, 0.0f, 1.0f, // 4
+        0.5f, 0.5f, -0.5f, 0.0f, 0.0f, 1.0f,  // 5
+        0.5f, 0.5f, 0.5f, 0.0f, 0.0f, 1.0f,   // 6
+        -0.5f, 0.5f, 0.5f, 0.0f, 0.0f, 1.0f,  // 7
+
+    };
 
     static constexpr auto triangleVertexIndices = std::array{
+        // front
         0, 1, 2, // first triangle
         2, 3, 0, // second triangle
+
+        // top
+        4, 5, 6, // first triangle
+        6, 7, 4, // second triangle
     };
 };
